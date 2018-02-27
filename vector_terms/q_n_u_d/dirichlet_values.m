38ea2918d073b4d198d2900ca8a35e2ab3d69d73
niravshah241 <niravshah.svnit@gmail.com>
2018-02-28 00:10:22 +0100

    Feb 27 2018

--- vector_terms/q_n_u_d/dirichlet_values.m
+++ vector_terms/q_n_u_d/dirichlet_values.m
@@ -15,31 +15,31 @@ y = glob(2);
 % end
 
 %% benchmark problem
-a = params.parameter_training_set(2); %factor for reynolds number
-if x>(-eps) & x<(0.1+eps) & y>(1-eps)
-    res = a*[10*x 0]';
-    
-elseif x>0.1 & x<(0.9+eps) & y>(1-eps)
-    res = a*[1 0]';
-    
-elseif x>0.9 & x<=1 & y>(1-eps)
-    res = a*[10-10*x 0]';
-    
-else
-    res = a*[0 0]';
-    
-end
+% a = 1e2; %factor for reynolds number
+% if x>(-eps) & x<(0.1+eps) & y>(1-eps)
+%     res = a*[10*x 0]';
+%     
+% elseif x>0.1 & x<(0.9+eps) & y>(1-eps)
+%     res = a*[1 0]';
+%     
+% elseif x>0.9 & x<=1 & y>(1-eps)
+%     res = a*[10-10*x 0]';
+%     
+% else
+%     res = a*[0 0]';
+%     
+% end
 
 %% standard
-% if glob(1)<eps
-%     res = 1e0*glob(2)*(1-glob(2));
-%     res = [res 0]';
-% 
-% elseif (glob(2)>(1-eps) || glob(2)<eps)
-%     res = [0 0]';
-% 
-% else
-%     res = [0 0]'; %on circular object
-% end
+if glob(1)<eps
+    res = 1e0*glob(2)*(1-glob(2));
+    res = [res 0]';
+
+elseif (glob(2)>(1-eps) || glob(2)<eps)
+    res = [0 0]';
+
+else
+    res = [0 0]'; %on circular object
+end
 
 end
--- vector_terms/t_v/neumann_values.m
+++ vector_terms/t_v/neumann_values.m
@@ -10,19 +10,18 @@ function [ res ] = neumann_values(glob,params)
 % end
 
 %% benchmark problem lid driven cavity
-b = params.parameter_training_set(3);
-if glob(2)<eps
-    res=b*[0 0]';
-else
-    error('Not on neumann boundary');
-end
-
-%% standard condition
-% if glob(1)>(1-eps)
+% if glob(2)<eps
 %     res=[0 0]';
 % else
-%     disp('Not on neumann boundary');
-%     %error('Not on neumann boundary');
+%     error('Not on neumann boundary');
 % end
+
+%% standard condition
+if glob(1)>(1-eps)
+    res=[0 0]';
+else
+    disp('Not on neumann boundary');
+    %error('Not on neumann boundary');
+end
 
 end
